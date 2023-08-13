ab4fb6b7aee1e47448a53454ae1cf5593834d117
David G Rosenberg <targendaz2@me.com>
2023-08-14 06:03:09 +0800

    Copied helpers from old package

---
+++ test/suite/helpers.ts
@@ -0,0 +1,41 @@
+import * as vscode from 'vscode';
+
+import * as cp from 'child_process';
+
+import * as utils from '../../src/utils';
+
+// type NodeEnvType = 'dev' | 'optional' | 'peer';
+
+// function npmList(npmPackage: string, include?: NodeEnvType[] | null, omit?: NodeEnvType[] | null): boolean {
+//     let command = `npm ls ${npmPackage}`;
+
+//     include?.forEach(nodeEnv => {
+//         command += ` --include ${nodeEnv}`;
+//     });
+
+//     omit?.forEach(nodeEnv => {
+//         command += ` --omit ${nodeEnv}`;
+//     });
+
+//     const result = cp.spawnSync(command, {
+//         'cwd': utils.getWorkspaceFolder().uri.fsPath,
+//         'shell': true
+//     });
+
+//     return (result.status === 0) ? true : false;
+// }
+
+// export function isPackageInstalled(npmPackage: string, nodeEnvs?: NodeEnvType[]): boolean {
+//     // If the package isn't installed in any env, this should return false
+//     // We include "dev" since setting NPM_ENV to "prod" omits it
+//     const installedAll = npmList(npmPackage, ['dev']);
+
+//     if (installedAll && nodeEnvs) {
+//         // Since the package *is* installed, we need to know if it's installed in the specified envs
+//         // If this returns true, the package is installed as a prod dependency, and this should return false
+//         const installedProd = npmList(npmPackage, null, nodeEnvs);
+//         return !installedProd;
+//     }
+
+//     return installedAll;
+// }
